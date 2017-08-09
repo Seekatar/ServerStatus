fc920174ae29d039935cca129e729fdff7b2b0d4
JimW <jim.wallace@versionone.com>
2017-08-09 13:10:30 -0400

    Add pending data to pipeline status

---
+++ Services/PendingDetails.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+
+namespace ServerStatus.Services
+{
+	/// <summary>
+	/// Details about a pending pipeline step
+	/// </summary>
+	public class PendingDetails
+	{
+		/// <summary>
+		/// Gets or sets the title.
+		/// </summary>
+		/// <value>
+		/// The title.
+		/// </value>
+		[JsonProperty(PropertyName ="title")]
+		public string Title { get; set; }
+		/// <summary>
+		/// Gets or sets the question.
+		/// </summary>
+		/// <value>
+		/// The question.
+		/// </value>
+		[JsonProperty(PropertyName ="question")]
+		public string Question { get; set; }
+		/// <summary>
+		/// Gets or sets the output key used internall by continuum.
+		/// </summary>
+		/// <value>
+		/// The output key.
+		/// </value>
+		[JsonProperty(PropertyName ="outputKey")]
+		public string OutputKey { get; set; }
+	}
+}
--- Services/PipelineStatus.cs
+++ Services/PipelineStatus.cs
@@ -1,4 +1,6 @@
-﻿namespace ServerStatus.Services
+﻿using Newtonsoft.Json;
+
+namespace ServerStatus.Services
 {
 	/// <summary>
 	/// bucket of status info about a pipeline
@@ -42,38 +44,52 @@
 		/// <summary>
 		/// the current phase if the instance is not complete
 		/// </summary>
+		[JsonProperty(PropertyName ="phase")]
 		public string Phase { get; set; }
 		/// <summary>
 		/// the current stage if the instance is not complete
 		/// </summary>
-		public string Stages { get; set; }
+		[JsonProperty(PropertyName ="stage")]
+		public string Stage { get; set; }
 		/// <summary>
 		/// the current step if the instance is not complete
 		/// </summary>
+		[JsonProperty(PropertyName ="step")]
 		public string Step { get; set; }
 		/// <summary>
 		/// the step index if the instance is not complete
 		/// </summary>
+		[JsonProperty(PropertyName ="stepIndex")]
 		public int StepIndex { get; set; }
 		/// <summary>
 		/// status of the item
 		/// </summary>
+		[JsonProperty(PropertyName ="status")]
 		public ItemStatus Status { get; set; }
 		/// <summary>
 		/// Total numbers of steps that may run
 		/// </summary>
+		[JsonProperty(PropertyName ="totalSteps")]
 		public int TotalSteps { get; internal set; }
 		/// <summary>
 		/// Total steps that ran 
 		/// </summary>
+		[JsonProperty(PropertyName ="totalSuccess")]
 		public int TotalSuccess { get; internal set; }
 		/// <summary>
 		/// Total steps skipped due to conditional statements
 		/// </summary>
+		[JsonProperty(PropertyName ="totalSkipped")]
 		public int TotalSkipped { get; internal set; }
 		/// <summary>
 		/// Total steps that failed
 		/// </summary>
+		[JsonProperty(PropertyName ="totalFailed")]
 		public int TotalFailed { get; internal set; }
+		/// <summary>
+		/// Details if the pipeline is pending
+		/// </summary>
+		[JsonProperty(PropertyName ="pending")]
+		public PendingDetails Pending { get; internal set; }
 	}
 }
